b4d4e1c0151275e0f6d7c4a6b95c9e8b8e7a1826
Anozys <Kazudu14@gmail.com>
2019-01-31 15:13:41 +0100

    31/01/2019 > 15:15 >> metter à jour

---
+++ php/accueil.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+		<title>Accueil</title>
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<p>Salut</p>
+				</div>
+
+				<div class="col-sm-4">
+					<p>Deuxième</p>
+				</div>
+
+				<div class="col-sm-4">
+					<p>Troisième</p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
